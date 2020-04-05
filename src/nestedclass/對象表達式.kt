f4d90547419e2a7de3241a1ef6b2a2d80634d6e2
czchen <louisa0103a@gmail.com>
2020-04-05 15:39:39 +0800

    object expression

--- src/nestedclass/對象表達式.kt
+++ src/nestedclass/對象表達式.kt
@@ -17,3 +17,71 @@ package nestedclass
 *  }
 * */
 
+interface MyInterface {
+    fun myPrint(i: Int)
+}
+
+abstract class MyAbstract {
+    abstract val age: Int
+
+    abstract fun printMyAbstractInfo()
+}
+
+fun main() {
+
+    //透過object關鍵字去實做MyInterface接口並改寫他的方法
+    val myObject = object : MyInterface {
+        override fun myPrint(i: Int) {
+            println("i的值是$i")
+        }
+    }
+    myObject.myPrint(100)
+    println("--------------")
+
+    //不實現任何接口也不繼承和抽象類的對象表達式的存在
+    val myObject2 = object {
+        init {
+            println("初始化塊")
+        }
+
+        var myProperty = "hello world"
+        fun myMethod() = "myMethod()"
+    }
+    println(myObject2.myProperty)
+    println(myObject2.myMethod())
+    println("--------------")
+
+    // 在Java裡面的匿名內部類只能實現一個抽象類或是多個接口.
+    // 在Kotlin解放了限制：他可以實作多個抽象類多個接口
+    var myObject3 = object : MyInterface, MyAbstract() {
+        override fun myPrint(i: Int) {
+            println("i的值是$i")
+        }
+
+        override val age: Int
+            get() = 30
+
+        override fun printMyAbstractInfo() {
+            println("printMyAbstractInfo invoked")
+        }
+    }
+    myObject3.myPrint(200)
+    println(myObject3.age)
+    myObject3.printMyAbstractInfo()
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/nestedclass/對象表達式3.kt
@@ -0,0 +1,27 @@
+package nestedclass
+
+/*
+* 匿名對象只能在局部變量範圍內或是被private修飾的成員變量範圍內才能被識別出其真正的類型
+* 如果將匿名對象當作一個public方法的返回類型或是public屬性的類型，那麼該方法或是屬性的真正類型
+* 就是該匿名對象所聲明的父類型，如果沒有聲明任何父類型，那麼其類型就是Any;
+* 在這種情況下，匿名中所聲明的任何成員都是無法訪問的
+* */
+
+class MyClass {
+    //去掉private會出錯，因為匿名類別是局部變量不能被外部所使用
+    private val myObject = object {
+        fun output() {
+            println("output invoked")
+        }
+    }
+
+    fun myTest() {
+        myObject.output()
+    }
+}
+
+fun main() {
+    var myClass = MyClass()
+    myClass.myTest()
+
+}
